9d029b1e6c3d782af2b11239d136d3a4f675f63e
James Scott <jmscott9@cps.edu>
2026-04-10 11:14:11 -0500

    4/10

--- a7.py
+++ a7.py
@@ -1,6 +1,8 @@
 import math, os, pickle, re
 from typing import Tuple, List, Dict
 
+# author: James Scott
+
 class BayesClassifier:
     """A simple BayesClassifier implementation
 
@@ -50,7 +52,6 @@ class BayesClassifier:
         _, __, files = next(os.walk(self.training_data_directory), (None, None, []))
         if not files:
             raise RuntimeError(f"Couldn't find path {self.training_data_directory}")
-        
 
         # files now holds a list of the filenames
         # self.training_data_directory holds the folder name where these files are
@@ -59,21 +60,17 @@ class BayesClassifier:
         # stored below is how you would load a file with filename given by `fName`
         # `text` here will be the literal text of the file (i.e. what you would see
         # if you opened the file in a text editor
-        # text = self.load_file(os.path.join(self.training_data_directory, files[2]))
-        # print(text)
+        # text = self.load_file(os.path.join(self.training_data_directory, fName))
+
 
         # *Tip:* training can take a while, to make it more transparent, we can use the
         # enumerate function, which loops over something and has an automatic counter.
         # write something like this to track progress (note the `# type: ignore` comment
         # which tells mypy we know better and it shouldn't complain at us on this line):
-        for index, filename in enumerate(files, 1): # type: ignore
-            print(f"Training on file {index} of {len(files)}")
+        # for index, filename in enumerate(files, 1): # type: ignore
+        #     print(f"Training on file {index} of {len(files)}")
         #     <the rest of your code for updating frequencies here>
-            print(f"{index}: {filename}")
-            text = self.load_file(os.path.join(self.training_data_directory, filename))
-            print(text)
-            tokens = self.tokenize(text)
-            print(tokens)
+
 
         # we want to fill pos_freqs and neg_freqs with the correct counts of words from
         # their respective reviews
@@ -85,7 +82,11 @@ class BayesClassifier:
         # positive frequency dictionary. If it is neither a postive or negative file,
         # ignore it and move to the next file (this is more just to be safe; we won't
         # test your code with neutral reviews)
-        
+            if filename.startswith(self.neg_file_prefix):
+                self.update_dict(tokens, self.neg_freqs)
+            elif filename.startswith(self.pos_file_prefix):
+                self.updtae_dict(tokens, self.pos_freqs)
+        print(self.pos_freqs)
 
         # Updating frequences: to update the frequencies for each file, you need to get
         # the text of the file, tokenize it, then update the appropriate dictionary for
@@ -93,6 +94,7 @@ class BayesClassifier:
         # your life easier here. Write that function first then pass it your list of
         # tokens from the file and the appropriate dictionary
         
+        # text = tokenize(self.update_dict)
 
         # for debugging purposes, it might be useful to print out the tokens and their
         # frequencies for both the positive and negative dictionaries
@@ -226,21 +228,28 @@ class BayesClassifier:
             freqs - dictionary of frequencies to update
         """
         # TODO: your work here
+        for word in words:
+            if word not in dict:
+                if word in freqs:
+                    freqs[word]+=1
+                else:
+                    freqs[word]=1
+
         pass  # remove this line once you've implemented this method
 
 
 if __name__ == "__main__":
     # uncomment the below lines once you've implemented `train` & `classify`
     b = BayesClassifier()
-    # a_list_of_words = ["I", "really", "like", "this", "movie", ".", "I", "hope", \
-    #                    "you", "like", "it", "too"]
-    # a_dictionary = {}
-    # b.update_dict(a_list_of_words, a_dictionary)
-    # assert a_dictionary["I"] == 2, "update_dict test 1"
-    # assert a_dictionary["like"] == 2, "update_dict test 2"
-    # assert a_dictionary["really"] == 1, "update_dict test 3"
-    # assert a_dictionary["too"] == 1, "update_dict test 4"
-    # print("update_dict tests passed.")
+    a_list_of_words = ["I", "really", "like", "this", "movie", ".", "I", "hope", \
+                        "you", "like", "it", "too"]
+    a_dictionary = {}
+    b.update_dict(a_list_of_words, a_dictionary)
+    assert a_dictionary["I"] == 2, "update_dict test 1"
+    assert a_dictionary["like"] == 2, "update_dict test 2"
+    assert a_dictionary["really"] == 1, "update_dict test 3"
+    assert a_dictionary["too"] == 1, "update_dict test 4"
+    print("update_dict tests passed.")
 
     # pos_denominator = sum(b.pos_freqs.values())
     # neg_denominator = sum(b.neg_freqs.values())
--- dict_wikipedia.py
+++ dict_wikipedia.py
@@ -2,15 +2,67 @@ import wikipedia
 import re, time
 from typing import List
 
+def tokenize(text:str)->List[str]:
+    """Splits given text into a list of the individual tokens in order
+    
+    Args:
+        text - text to tokenize
+        
+    Returns:
+        tokens of given text in order
+        
+    """
+
+    tokens = []
+    token = ""
+    for c in text:
+        if (
+            re.match("[a-zA-Z0-9]", str(c)) !=None
+            or c == "'"
+            or c == "__"
+            or c == "_"
+        ):
+            token += c
+        else:
+            if token !="":
+                tokens.append(token.lower())
+                token = ""
+
+    if token != "":
+        tokens.append(token.lower())
+    return tokens
+
 article = wikipedia.page("Artemis II", auto_suggest=False).content
-print(article)
+# print(article)
+words = tokenize(article)
+
+with open("sorted_stoplist.txt", "r", encoding = "utf8") as f:
+    stoplist = f.read()
+tokenize_stoplist = tokenize(stoplist)
+# print(tokenize_stoplist) 
 
 freqs = {}
 
-for word in tokens:
-    if word in freqs:
-        freqs[word] += 1
-    else:
-        freqs[word] = 1
+for word in words:
+    if word not in tokenize_stoplist:
+        if word in freqs:
+            freqs[word]+= 1
+        else:
+            freqs[word] = 1
 
 print(freqs)
+
+# Print out the number of unique words
+total_unique_words = len(freqs)
+print(f"Unique words: {total_unique_words}")
+
+# Print out the total number of words
+total_words = sum(freqs.values())
+print(f"Total words: {total_words}")
+
+print (freqs)
+# Print the top 20 words
+top_words = sorted(freqs.items(), key=lambda x: x[1], reverse = True)
+print ("Top 20 words in content")
+for word, count in top_words[:20]:
+    print(f"{word} {count}")
